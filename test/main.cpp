358a0acbad60247205fb1bcb8db21a377dfe5b8e
H. Watanabe <kaityo@users.sourceforge.jp>
2020-02-04 14:39:23 +0900

    udpates main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -5,7 +5,8 @@ std::mt19937 mt;
 const int N = 65536;
 
 int spin[N] = {};
-std::uniform_real_distribution<> ud(0.0, 1.0);
+//std::uniform_real_distribution<> ud(0.0, 1.0);
+std::uniform_int_distribution<> ud(0.0, 1.0);
 
 void run() {
   for (int i = 0; i < N; i++) {
@@ -18,4 +19,5 @@ int main() {
   for (int j = 0; j <1000; j++) {
     run();
   }
+  //std::cout << ud(mt) << std::endl;
 }
