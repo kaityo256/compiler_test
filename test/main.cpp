dae28d9f0c416cb39940356da9115f0e43a593cb
H. Watanabe <kaityo@users.sourceforge.jp>
2020-02-04 15:07:25 +0900

    updates main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -2,22 +2,15 @@
 #include <random>
 
 std::mt19937 mt;
-const int N = 65536;
 
-int spin[N] = {};
-//std::uniform_real_distribution<> ud(0.0, 1.0);
-std::uniform_int_distribution<> ud(0.0, 1.0);
-
-void run() {
-  for (int i = 0; i < N; i++) {
-    if (spin[i] != 0) continue;
-    ud(mt);
-  }
-}
+std::uniform_real_distribution<> ud(0.0, 1.0);
+//std::uniform_int_distribution<> ud(0.0, 1.0);
 
 int main() {
   for (int j = 0; j <1000; j++) {
-    run();
+    for (int i = 0; i < 65536; i++) {
+      if (i%2) ud(mt);
+    }
   }
   //std::cout << ud(mt) << std::endl;
 }
