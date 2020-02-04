5889e56c8588beee5e82e4f81c187f9c0e6be571
H. Watanabe <kaityo@users.sourceforge.jp>
2020-02-04 15:09:43 +0900

    udpates main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
 #include <random>
-
-std::mt19937 mt;
-
-std::uniform_real_distribution<> ud(0.0, 1.0);
-//std::uniform_int_distribution<> ud(0.0, 1.0);
-
 int main() {
-  for (int j = 0; j <1000; j++) {
-    for (int i = 0; i < 65536; i++) {
+  std::mt19937 mt;
+  std::uniform_real_distribution<> ud(0.0, 1.0);
+  for (int j = 0; j <10000; j++) {
+    for (int i = 0; i < 10000; i++) {
       if (i%2) ud(mt);
     }
   }
-  //std::cout << ud(mt) << std::endl;
 }
